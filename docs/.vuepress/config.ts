a51dbd021f04c638473675f77de4636117f36c5d
anyu1234 <597248157@qq.com>
2023-03-07 21:01:51 +0800

    修改发布路径

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,6 +1,7 @@
 import { defaultTheme, defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
+    base: '/nuxtjs-docs/',
     lang: 'zh-CN',
     title: 'Nuxtjs',
     description: '阅读 Nuxt.js 官方文档',
