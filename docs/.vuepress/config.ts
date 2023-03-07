24508929856fa5d61c34f1f398711cd28b595319
anyu1234 <597248157@qq.com>
2023-03-07 21:20:04 +0800

    修改配置base

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,7 +1,7 @@
 import { defaultTheme, defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
-    base: '/nuxtjs-docs/',
+    base: '/vuepress/',
     lang: 'zh-CN',
     title: 'Nuxtjs',
     description: '阅读 Nuxt.js 官方文档',
