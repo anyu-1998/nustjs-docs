95d732cc6cb44dabfd5dc5adac475932d92da9d7
anyu1234 <597248157@qq.com>
2023-03-07 15:43:09 +0800

    更改图片

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -4,6 +4,9 @@ export default defineUserConfig({
     lang: 'zh-CN',
     title: 'Nuxtjs',
     description: '阅读 Nuxt.js 官方文档',
+    head: [
+        ['link', { rel: 'icon', href: '/logo.svg' }]
+    ],
     theme: defaultTheme({
         logo: '/logo.svg',
         // 默认主题配置
